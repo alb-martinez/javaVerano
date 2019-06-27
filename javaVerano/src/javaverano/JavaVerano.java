808c601c29a36e2740f639aa8d3e8ed604235bad
Alberto Martinez Ruano <37654975+alb-martinez@users.noreply.github.com>
2019-06-27 19:17:51 +0200

    Update JavaVerano.java
    modificación desde github

--- javaVerano/src/javaverano/JavaVerano.java
+++ javaVerano/src/javaverano/JavaVerano.java
@@ -15,7 +15,7 @@ public class JavaVerano {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola github");
+        System.out.println("Hola netbeans");
     }
     
 }

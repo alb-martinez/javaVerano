fd1197d52c5d2b07e6d159e10dc9ef4c4f988fa8
Alberto Martinez Ruano <almartinez.amr@gmail.com>
2019-08-16 11:31:02 +0200

    añadido ejercicio 2.2-conversor libras/euros con redondeo

--- javaVerano/src/javaverano/ejercicio2_2/ConversorPrueba.java
+++ javaVerano/src/javaverano/ejercicio2_2/ConversorPrueba.java
@@ -1,15 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaverano.ejercicio2_2;
-import java.io.*;
 import java.util.Scanner;
 
 /**
  *
- * @author Alberto
+ * @author Alberto Martinez
  */
 public class ConversorPrueba {
     
